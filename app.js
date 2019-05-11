31589c7244dc8506da3fcb43cdae197010b22995
phanendery <phanendery@gmail.com>
2019-05-11 09:55:24 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const express = require("express");
+const app = express();
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Mrs.SnaccMan coming to you soon!"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
