c85fe766f60fb21d8282d1a692f3e210bf079c91
Aidan Skye <aidanskye42@gmail.com>
2019-05-11 15:14:34 -0700

    Basic rendering of a grid, util file to translate between logical size and pixel size

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import Game from "./components/game";
 
 document.addEventListener("DOMContentLoaded",()=>{
-  ReactDOM.render(<div>React Loaded</div>, document.querySelector("#root"));
+  ReactDOM.render(<Game />, document.querySelector("#root"));
 });
