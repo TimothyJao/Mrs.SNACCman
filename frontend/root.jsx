f78bfc8026dba281e938350bb9374113528cd27a
Aidan Skye <aidanskye42@gmail.com>
2019-05-11 12:58:28 -0700

    Initial frontend/react stuff

---
+++ frontend/root.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+document.addEventListener("DOMContentLoaded",()=>{
+  ReactDOM.render(<div>React Loaded</div>, document.querySelector("#root"));
+});
