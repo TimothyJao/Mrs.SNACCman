b5b1e869816dceeb4f329d9f9d4215f36d221bd1
Yepez1997 <yepez4845@gmail.com>
2019-05-12 13:15:13 -0700

    Adding more

--- classes/Ghost.js
+++ classes/Ghost.js
@@ -13,6 +13,17 @@ class Ghost extends Entity{
         this.velocity = velocity;
     }
 
+
+    /* getPred - returns previous elements in order */ 
+    getPredInOrder(pred) {
+        /* each elt in pred is an object -> extract x and y coordinates from each pred */
+        let predOrder = [];
+        for (let i = 0; i < pred.length; i++) {
+            predOrder.unshift([pred[i].x,pred[i].y])
+        }
+        return predOrder;
+    }
+
     /**setUpVisitedNodes - returns a boolean matrix of valid nodes a ghost can traverse */
     setUpVisitedNodes(grid) {
         let visited = grid;
@@ -36,7 +47,8 @@ class Ghost extends Entity{
         /* set up valid positions that the ghost can travel to */
         visited_grid = setUpVisitedNodes(grid);
         /* pred - holds the previous objects the ghost has traveled to */
-        let pred = [[].[]];
+        //let pred = [[].[]];
+        let pred = []
         let queue = [];
 
          /* bfs traversal */
@@ -51,13 +63,13 @@ class Ghost extends Entity{
                     /* update values for shortest path -> by default we dont explore the X's - ie the grids that the ai should not go through*/
                     visited[grid[x][i].x][grid[x][i].y] = true;
                     // pred[grid[x[i].x][grid[x[i].y] = grid[x[x;
-                    pred[grid[x][i].x][grid[x][i].y].push(grid[x][y]);
+                    // pred[grid[x][i].x][grid[x][i].y].push(grid[x][y]);
                     let nextToQueue = [grid[x][i].x, grid[x][i].y]
                     queue.push(nextToQueue);
+                    pred.push(grid[x][i])
                     /* if the coordinates match */
                     if ([grid[x][i].x, grid[x][i].y] === snacManPos) {
-                        // TODO: extract all coordinates from the objects in pred - *** in order ***
-                        return pred;
+                        return getPredInOrder(pred);
                     }
                 }
             }
