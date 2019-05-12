0594dd224d33c05e2fa6f85b319ba8bd68a648d4
Yepez1997 <yepez4845@gmail.com>
2019-05-12 14:49:30 -0700

    adding socket

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ var http = require('http').createServer(app);
 
 // const express = require("express");
 // const app = express();
-const db = require("./config/keys").mongoURI;
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+// const db = require("./config/keys").mongoURI;
+// mongoose
+//   .connect(db, { useNewUrlParser: true })
+//   .then(() => console.log("Connected to MongoDB successfully"))
+//   .catch(err => console.log(err));
 
-  
-app.get("/", (req, res) => res.send("Mrs.SnaccMan coming to you soon!"));
+/* read the index.html file to host on localhost::5000 */
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 const port = process.env.PORT || 5000;
-http.listen(port, () => console.log(`Server is running on port ${port}`));
+http.listen(port, () => console.log(`Listening on port ${port}`));
