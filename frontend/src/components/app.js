1a0013cba2ee0fb6cb67af5d1d65be0dd099f2d5
phanendery <phanendery@gmail.com>
2019-05-13 20:31:27 -0700

    fixing hopefully

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,17 +2,17 @@
 
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-import MainPage from "./main/main_page";
+import MainPageContainer from "./main/main_page_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
   <Switch>
-    <AuthRoute exact path="/" component={MainPage} />
-    <AuthRoute exact path="/login" component={LoginFormContainer} />
-    <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    <Route exact path="/" component={MainPageContainer} />
+    <Route exact path="/login" component={LoginFormContainer} />
+    <Route exact path="/signup" component={SignupFormContainer} />
   </Switch>
 );
 
--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
 class LoginForm extends React.Component {
   constructor(props) {
