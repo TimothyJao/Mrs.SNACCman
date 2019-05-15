9171e06a80ccbf00105580b2e145da4ed7d7e642
Aidan Skye <aidanskye42@gmail.com>
2019-05-14 17:12:50 -0700

    Start refactoring

--- frontend/src/singleplayer.jsx
+++ frontend/src/singleplayer.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {SNACCMAN, GHOST} from "./classes/Entity";
-import Grid from "./classes/Grid";
-import Ghost from "./classes/Ghost";
-import Snaccman from "./classes/Snaccman";
+
 import Game from "./components/game";
 
 document.addEventListener("DOMContentLoaded",()=>{
-  const grid = new Grid();
-  const snaccman = new Snaccman(12.5, 22, SNACCMAN, [1, 0]);
-  const ghosts = [
-    new Ghost(12, 12, GHOST, [0, -1]),
-    new Ghost(12, 13, GHOST, [0, -1]),
-    new Ghost(13, 12, GHOST, [0, -1]),
-    new Ghost(13, 13, GHOST, [0, -1])
-  ];
+  //currentPlayer: 0 = snaccman, 1-4 = ghosts; if omitted, will default to snaccman
+  //numberOfPlayers: How many players are playing, the rest will be AI controlled; will default to 1 if ommitted
   ReactDOM.render(
-    <Game grid={grid.getMoveGrid()} ghosts={ghosts} snaccman={snaccman} pellets={grid.getPelletGrid()} />,
+    <Game currentPlayer={0} numberOfPlayers={1} />,
     document.querySelector("#root")
   );
 });
